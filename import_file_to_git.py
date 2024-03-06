e204ce3f19927240c37053a3d1c915388797c1c6
Anton <antonio1605belozercev@mail.ru>
2024-03-06 20:40:17 +0500

    Revert "создал второй файл для теста функции"
    This reverts commit 217c42a2fac65a1f6ef82df6a4859caac2f486c1.

--- import_file_to_git.py
+++
@@ -1,8 +0,0 @@
-from git_repository_new_file import mathematic
-import unittest
-
-class NameTestCase(unittest.TestCase):
-
-    def test_mathematic(self):
-        res = mathematic(7,9)
-        self.assertEqual(res,16)
